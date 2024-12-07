54e0ca850625a5c69641392db94e291f92692736
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-07 18:47:22 +0500

    Sprint5

---
+++ Tyuiu.DmitrievLR.Sprint5.Task0.V11.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO; // для исполльзования FILE
+
+
+namespace Tyuiu.DmitrievLR.Sprint5.Task0.V11.Lib
+{
+    public class DataService : ISprint5Task0V11
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
+
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
+            double y = (4 - Math.Pow(x, 3)) / Math.Pow(x, 2);
+
+            var result = Math.Pow(y, 3);
+
+
+            File.WriteAllText(filePath, y.ToString());
+            File.WriteAllText(path, Convert.ToString(result));
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint5.Task0.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.DmitrievLR.Sprint5.Task0.V11.Lib;
+namespace Tyuiu.DmitrievLR.Sprint5.Task0.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            DataService ds = new DataService();
+            string res = ds.SaveToFileTextData(x);
+
+
+            Console.WriteLine("Файл " + res + " создан.");
+            Console.ReadKey();
+        }
+    }
+}
