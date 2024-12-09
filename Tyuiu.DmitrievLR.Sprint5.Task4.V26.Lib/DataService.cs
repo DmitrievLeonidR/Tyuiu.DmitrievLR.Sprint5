76954a6d52cb2ae73bdf67dc7d1f26483cb732b8
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-09 13:55:53 +0500

    Task4

---
+++ Tyuiu.DmitrievLR.Sprint5.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DmitrievLR.Sprint5.Task4.V26.Lib
+{
+    public class DataService : ISprint5Task4V26
+    {
+        public double LoadFromDataFile(string path)// ЧТЕНИЕ ИЗ ЗАПИСАННОГО ФАЙЛА
+        {
+            string strX = File.ReadAllText(path);
+            double d = Convert.ToDouble(strX);
+            double result = Math.Pow(d, 3) + Math.Sin(d) * Math.Cos(d);
+            double res = Math.Round(result, 3);
+
+            return res;
+        }
+    } 
+}
---
+++ Tyuiu.DmitrievLR.Sprint5.Task4.V26/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.DmitrievLR.Sprint5.Task4.V26.Lib;
+namespace Tyuiu.DmitrievLR.Sprint5.Task4.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutFileTask4.txt";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.LoadFromDataFile(path);
+            Console.WriteLine(result);
+            Console.ReadKey(); 
+        }
+    }
+}
